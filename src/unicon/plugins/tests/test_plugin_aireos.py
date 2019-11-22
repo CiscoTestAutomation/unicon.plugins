0a7d30f54d5b5bfa6647e11fce5cd8d508342e02
Dave Wapstra <dwapstra@cisco.com>
2019-11-23 09:55:13 +1100

    Use old style formatting

--- src/unicon/plugins/tests/test_plugin_aireos.py
+++ src/unicon/plugins/tests/test_plugin_aireos.py
@@ -16,7 +16,7 @@ hostname = 'Cisco Capwap Simulator'
 box['hostname'] = hostname
 box['username'] = 'lab'
 box['tacacs_password'] = 'lab'
-box['cmd'] = f'mock_device_cli --os aireos  --state aireos_exec --hostname "{hostname}"'
+box['cmd'] = 'mock_device_cli --os aireos  --state aireos_exec --hostname "{}"'.format(hostname)
 
 ping_failure = '192.168.2.3'
 
@@ -212,11 +212,11 @@ class TestAireOsPlugin(unittest.TestCase):
 
     def setUp(self):
         self.c = Connection(hostname='Cisco Capwap Simulator',
-                            start=[f'mock_device_cli --os aireos  --state aireos_exec --hostname "{hostname}"'],
+                            start=['mock_device_cli --os aireos  --state aireos_exec --hostname "{}"'.format(hostname)],
                             os='aireos',
                             username='lab')
         self.cc = Connection(hostname='Cisco Capwap Simulator',
-                             start=[f'mock_device_cli --os aireos  --state aireos_exec --hostname "{hostname}"'],
+                             start=['mock_device_cli --os aireos  --state aireos_exec --hostname "{}"'.format(hostname)],
                              os='aireos',
                              credentials=dict(default=dict(username='lab', password='lab')))
 
@@ -247,7 +247,7 @@ class TestAireOsPlugin(unittest.TestCase):
 class TestAireOsPluginLearnHostname(unittest.TestCase):
     def test_learn_hostname(self):
         c = Connection(hostname='Controller',
-                       start=[f'mock_device_cli --os aireos  --state aireos_exec --hostname "{hostname}"'],
+                       start=['mock_device_cli --os aireos  --state aireos_exec --hostname "{}"'.format(hostname)],
                        os='aireos',
                        username='lab',
                        learn_hostname=True,
@@ -259,7 +259,7 @@ class TestAireosPluginStates(unittest.TestCase):
 
     def setUp(self):
         self.c = Connection(hostname='Controller',
-                            start=[f'mock_device_cli --os aireos  --state aireos_exec --hostname Controller'],
+                            start=['mock_device_cli --os aireos  --state aireos_exec --hostname Controller'],
                             os='aireos',
                             username='lab',
                             init_config_commands=[])
