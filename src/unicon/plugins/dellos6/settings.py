04be549910773ec523986a93ed6740344667a769
Knox Hutchinson <knox@knoxsdata.com>
2020-12-17 19:04:06 +0000

    dellos6 connection cleanup

--- src/unicon/plugins/dellos6/settings.py
+++ src/unicon/plugins/dellos6/settings.py
@@ -15,3 +15,10 @@ class DellosSettings(GenericSettings):
         super().__init__()
         self.CONNECTION_TIMEOUT = 60*5
         self.ESCAPE_CHAR_CALLBACK_PRE_SENDLINE_PAUSE_SEC = 1
+        self.HA_INIT_EXEC_COMMANDS = [
+            'term length 0',
+            'show version'
+        ]
+        self.HA_INIT_CONFIG_COMMANDS = [
+            'no logging console'
+        ]
