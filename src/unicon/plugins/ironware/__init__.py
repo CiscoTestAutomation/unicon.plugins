5ec24fe78767b6257ba43f0b175fda069cc007e8
James Di Trapani <51264648+jamesditrapani@users.noreply.github.com>
2020-12-28 20:06:30 +1000

    [ironware] MPLS Ping Service & Test Changes - Added a MPLS Ping service to the IronWare plugin - Added tests associated with a MPLS LSP Ping Success/Failure - Moved credential handling in tests to 'credentials' to migrate away from depreciated items - Moved services into service_implementation.py - Moved service list definition to __init__.py - Removed services.py

--- src/unicon/plugins/ironware/__init__.py
+++ src/unicon/plugins/ironware/__init__.py
@@ -10,14 +10,22 @@ Description:
 """
 
 from unicon.bases.routers.connection import BaseSingleRpConnection
-from unicon.plugins.generic import GenericSingleRpConnectionProvider
-from .statemachine import IronWareSingleRpStateMachine
-from .services import IronWareServiceList
-from .settings import IronWareSettings
+from unicon.plugins.generic import GenericSingleRpConnectionProvider, ServiceList
 
+from .statemachine import IronWareSingleRpStateMachine
+from .settings import IronWareSettings
+from unicon.plugins.ironware import service_implementation
+
+
+class IronWareServiceList(ServiceList):
+    def __init__(self):
+        super().__init__()
+        self.execute = service_implementation.Execute
+        self.mpls_ping = service_implementation.MPLSPing
 
 class IronWareSingleRPConnection(BaseSingleRpConnection):
     os = 'ironware'
+    series = None
     chassis_type = 'single_rp'
     state_machine_class = IronWareSingleRpStateMachine
     connection_provider_class = GenericSingleRpConnectionProvider
--- src/unicon/plugins/tests/test_plugin_ironware.py
+++ src/unicon/plugins/tests/test_plugin_ironware.py
@@ -7,6 +7,7 @@ import unicon
 from unicon import Connection
 from unicon.eal.dialogs import Dialog
 from unicon.mock.mock_device import mockdata_path
+from unicon.core.errors import SubCommandFailure
 
 with open(os.path.join(mockdata_path, 'ironware/ironware_mock_data.yaml'), 'rb') as datafile:
     mock_data = yaml.safe_load(datafile.read())
@@ -18,8 +19,15 @@ class TestIronWarePluginConnect(unittest.TestCase):
         c = Connection(hostname='mlx8',
                         start=['mock_device_cli --os ironware --state exec'],
                         os='ironware',
-                        username='ironware1',
-                        tacacs_password='pyatsRocks!')
+                        credentials={
+                            'default': {
+                                'username': 'ironware1',
+                                'password': 'pyatsRocks!'
+                            },
+                            'enable': {
+                                'password': 'pyatsRocks!'
+                            }
+                        })
         c.connect()
         self.assertIn('mlx8#', c.spawn.match.match_output)
 
@@ -27,8 +35,15 @@ class TestIronWarePluginConnect(unittest.TestCase):
         c = Connection(hostname='mlx8',
                         start=['mock_device_cli --os ironware --state connect_ssh'],
                         os='ironware',
-                        username='ironware1',
-                        tacacs_password='pyatsRocks!')
+                        credentials={
+                            'default': {
+                                'username': 'ironware1',
+                                'password': 'pyatsRocks!'
+                            },
+                            'enable': {
+                                'password': 'pyatsRocks!'
+                            }
+                        })
         c.connect()
         self.assertIn('mlx8#', c.spawn.match.match_output)
 
@@ -36,8 +51,15 @@ class TestIronWarePluginConnect(unittest.TestCase):
         c = Connection(hostname='mlx8',
                         start=['mock_device_cli --os ironware --state exec'],
                         os='ironware',
-                        username='ironware1',
-                        tacacs_password='pyatsRocks!',
+                        credentials={
+                            'default': {
+                                'username': 'ironware1',
+                                'password': 'pyatsRocks!'
+                            },
+                            'enable': {
+                                'password': 'pyatsRocks!'
+                            }
+                        },
                         connect_reply = Dialog([[r'^(.*?)Password:']]))
         c.connect()
         self.assertIn("^(.*?)Password:", str(c.connection_provider.get_connection_dialog()))
@@ -49,8 +71,15 @@ class TestIronWarePluginExecute(unittest.TestCase):
         c = Connection(hostname='mlx8',
                         start=['mock_device_cli --os ironware --state exec'],
                         os='ironware',
-                        username='ironware1',
-                        tacacs_password='pyatsRocks!',
+                        credentials={
+                            'default': {
+                                'username': 'ironware1',
+                                'password': 'pyatsRocks!'
+                            },
+                            'enable': {
+                                'password': 'pyatsRocks!'
+                            }
+                        },
                         init_exec_commands=[],
                         init_config_commands=[]
                         )
@@ -59,6 +88,50 @@ class TestIronWarePluginExecute(unittest.TestCase):
         expected_response = mock_data['exec']['commands'][cmd].strip()
         ret = c.execute(cmd).replace('\r', '')
         self.assertIn(expected_response, ret)
+
+class TestIronWarePluginMPLSPing(unittest.TestCase):
+
+    def test_mpls_ping_success(self):
+        c = Connection(hostname='mlx8',
+                            start=['mock_device_cli --os ironware --state exec'],
+                            os='ironware',
+                            credentials={
+                                'default': {
+                                    'username': 'ironware1',
+                                    'password': 'pyatsRocks!'
+                                },
+                                'enable': {
+                                    'password': 'pyatsRocks!'
+                                }
+                            })
+        c.mpls_ping(lsp='mlx8.1_to_ces.2')
+        self.assertEqual("\n".join(c.spawn.match.match_output.splitlines()),"""ping mpls rsvp lsp mlx8.1_to_ces.2
+Send 5 96-byte MPLS Echo Requests over RSVP LSP mlx8.1_to_ces.2, timeout 5000 msec
+Type Control-c to abort
+!!!!!
+Success rate is 100 percent (5/5), round-trip min/avg/max=0/1/3 ms
+mlx8#""")
+
+    def test_mpls_ping_failure(self):
+        c = Connection(hostname='mlx8',
+                            start=['mock_device_cli --os ironware --state exec'],
+                            os='ironware',
+                            credentials={
+                                'default': {
+                                    'username': 'ironware1',
+                                    'password': 'pyatsRocks!'
+                                },
+                                'enable': {
+                                    'password': 'pyatsRocks!'
+                                }
+                            })
+        try:
+            c.mpls_ping(lsp='mlx8.1_to_mlx8.4')
+        except SubCommandFailure:
+            pass
+        self.assertEqual("\n".join(c.spawn.match.match_output.splitlines()),"""ping mpls rsvp lsp mlx8.1_to_mlx8.4
+Ping fails: LSP is down
+mlx8#""")
 
 if __name__ == "__main__":
     unittest.main()
