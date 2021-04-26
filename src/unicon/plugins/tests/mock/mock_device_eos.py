c02f9c4fc3f6c0b83e92b475feeaa2acf4c0dac6
Richard Day <richardday89@gmail.com>
2021-04-26 15:34:46 +0100

    Fixing EOS tests to work with the --hostname argument

--- src/unicon/plugins/tests/mock/mock_device_eos.py
+++ src/unicon/plugins/tests/mock/mock_device_eos.py
@@ -37,7 +37,7 @@ def main(args=None):
     if not args:
         parser = argparse.ArgumentParser()
         parser.add_argument('--state', help='initial state')
-        parser.add_argument('--hostname', help='Device hostname (default: Router')
+        parser.add_argument('--hostname', help='Device hostname (default: Switch')
         parser.add_argument('-d', action='store_true', help='Debug')
         args = parser.parse_args()
 
@@ -45,7 +45,7 @@ def main(args=None):
         logging.getLogger(__name__).setLevel(logging.DEBUG)
 
     state = args.state or 'exec'
-    hostname = args.hostname or 'Router'
+    hostname = args.hostname or 'Switch'
     md = MockDeviceEOS(hostname=hostname, state=state)
     md.run()
 
