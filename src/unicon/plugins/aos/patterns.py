4fe4d2e2c8bab7a9e36d3253ec68117d5a410a6b
Alex Pfeil <apfeil@amfam.com>
2022-06-22 14:47:14 -0400

    commit

--- src/unicon/plugins/aos/patterns.py
+++ src/unicon/plugins/aos/patterns.py
@@ -16,3 +16,51 @@ class aosPatterns():
         self.config_mode = r'\w+.config.#'
         self.password = r'\w+.*[Pp]assword:'
         self.linePassword = r'\w+.*[Pp]assword:'
+# self.enable_prompt = r'.*%N#\s?$'
+        self.default_hostname_pattern = r'WLC|RouterRP|Router|[Ss]witch|Controller|ios'
+
+        self.rommon_prompt = r'^(.*?)(rommon[\s\d]*>|switch:)\s?$'
+        # self.standby_enable_prompt = r'^(.*?)(RouterRP-standby|%N-standby|%N-sdby|%N\(standby\))#\s?$'
+        # self.standby_disable_prompt = r'^(.*?)(RouterRP-standby|%N-standby|%N-sdby|%N\(standby\))>\s?$'
+        self.standby_locked = r'[S|s]tandby console disabled'
+        self.shell_prompt = r'^(.*)%N\(shell\)>\s?'
+
+        self.disconnect_message = r'Received disconnect from .*:'
+
+        self.continue_connect = r'Are you sure you want to continue connecting \(yes/no(/\[fingerprint\])?\)'
+
+        self.cisco_commit_changes_prompt = r'Uncommitted changes found, commit them\? \[yes/no/CANCEL\]'
+        self.juniper_commit_changes_prompt = r'Discard changes and continue\? \[yes,no\]'
+
+        self.hit_enter = r'Hit Enter to proceed:'
+        self.press_ctrlx = r"^(.*?)Press Ctrl\+x to Exit the session"
+        self.connected = r'^(.*?)Connected.'
+
+        self.enter_basic_mgmt_setup = r'Would you like to enter basic management setup\? \[yes/no\]:'
+        self.kerberos_no_realm = r'^(.*)Kerberos:\s*No default realm defined for Kerberos!'
+
+        self.passphrase_prompt = r'^.*Enter passphrase for key .*?:\s*?'
+
+        self.learn_os_prompt = r'^(.*?([>\$~%]|[^#\s]#|~ #|~/|^admin:|^#)\s?(\x1b\S+)?)$|(^.*This \(D\)RP Node is not ready or active for login \/configuration.*)'
+
+        self.sudo_password_prompt = r'^.*\[sudo\] password for .*?:\s*?'
+
+        # *Sep 6 23:13:38.188: %PNP-6-PNP_SDWAN_STARTED: PnP SDWAN started (7) via (pnp-sdwan-abort-on-cli) by (pid=3, pname=Exec)
+        # *Sep 6 23:18:11.702: %ENVIRONMENTAL-1-ALERT: Temp: Inlet 1, Location: R0, State: Warning, Reading: 45 Celsius
+        # *Sep 6 17:43:41.291: %Cisco-SDWAN-RP_0-CFGMGR-4-WARN-300005: New admin password not set yet, waiting for daemons to read initial config.
+        self.syslog_message_pattern = r'^.*?%\w+(-\S+)?-\d+-\w+.*$'
+
+        self.config_locked = r'Configuration (mode )?(is )?locked|Config mode cannot be entered'
+
+        self.config_start = r'Enter configuration commands, one per line\.\s+End with CNTL/Z\.\s*$'
+
+        self.enable_secret = r'^.*?(Enter|Confirm) enable secret:\s*$'
+
+        self.enter_your_selection_2 = r'^.*?Enter your selection( \[2])?:\s*$'
+
+        self.guestshell_prompt = r'^(.*)\[\S+@guestshell\s+.*\][#\$]\s?$'
+
+        self.press_any_key = r'^.*?Press any key to continue\.\s*$'
+
+        # VT100 patterns
+        self.get_cursor_position = r'\x1b\[6n'
--- src/unicon/plugins/aos/statements.py
+++ src/unicon/plugins/aos/statements.py
@@ -6,11 +6,12 @@ https://github.com/CiscoDevNet/pyats-plugin-examples/tree/master/unicon_plugin_e
 '''
 from unicon.eal.dialogs import Statement
 from unicon.plugins.generic.statements import GenericStatements
-from unicon.plugins.aos.patterns import aosPatterns
+from .patterns import aosPatterns
 from unicon.plugins.generic.statements import password_handler
 
 statements = GenericStatements()
 patterns = aosPatterns()
+patterns = pat
 
 def login_handler(spawn, context, session):
     spawn.sendline(context['\r'])
@@ -52,9 +53,173 @@ password_stmt = Statement(pattern=patterns.password,
                           continue_timer=True,
                           trim_buffer=True)
 
-login_password = Statement(pattern=patterns.linePassword,
-                           action=line_password_handler,
+escape_char_stmt = Statement(pattern=patterns.escape_char,
+                             action=escape_char_callback,
+                             args=None,
+                             loop_continue=True,
+                             continue_timer=False)
+press_return_stmt = Statement(pattern=patterns.press_return,
+                              action=wait_and_enter, 
+                              args=None,
+                              loop_continue=True,
+                              continue_timer=False)
+connection_refused_stmt = \
+    Statement(pattern=patterns.connection_refused,
+              action=connection_refused_handler,
+              args=None,
+              loop_continue=False,
+              continue_timer=False)
+bad_password_stmt = Statement(pattern=patterns.bad_passwords,
+                              action=bad_password_handler,
+                              args=None,
+                              loop_continue=False,
+                              continue_timer=False)
+
+login_incorrect = Statement(pattern=patterns.login_incorrect,
+                            action=incorrect_login_handler,
+                            args=None,
+                            loop_continue=True,
+                            continue_timer=False)
+
+disconnect_error_stmt = Statement(pattern=patterns.disconnect_message,
+                                  action=connection_failure_handler,
+                                  args=None,
+                                  loop_continue=False,
+                                  continue_timer=False)
+useraccess_stmt = Statement(pattern=patterns.useracess,
+                                 action=user_access_verification,
+                                 args=None,
+                                 loop_continue=True,
+                                 continue_timer=False)
+password_stmt = Statement(pattern=patterns.password,
+                          action=password_handler,
+                          args=None,
+                          loop_continue=True,
+                          continue_timer=False)
+enable_password_stmt = Statement(pattern=patterns.password,
+                                 action=enable_password_handler,
+                                 args=None,
+                                 loop_continue=True,
+                                 continue_timer=False)
+enable_secret_stmt = Statement(pattern=patterns.enable_secret,
+                               action=enable_password_handler,
+                               args=None,
+                               loop_continue=True,
+                               continue_timer=False)
+password_ok_stmt = Statement(pattern=patterns.password_ok,
+                             action=sendline,
+                             args=None,
+                             loop_continue=True,
+                             continue_timer=False)
+more_prompt_stmt = Statement(pattern=patterns.more_prompt,
+                             action=more_prompt_handler,
+                             args=None,
+                             loop_continue=True,
+                             continue_timer=False)
+confirm_prompt_stmt = Statement(pattern=patterns.confirm_prompt,
+                                action=sendline,
+                                args=None,
+                                loop_continue=True,
+                                continue_timer=False)
+confirm_prompt_y_n_stmt = Statement(pattern=patterns.confirm_prompt_y_n,
+                                    action='sendline(y)',
+                                    args=None,
+                                    loop_continue=True,
+                                    continue_timer=False)
+yes_no_stmt = Statement(pattern=patterns.yes_no_prompt,
+                        action=sendline,
+                        args={'command': 'y'},
+                        loop_continue=True,
+                        continue_timer=False)
+
+continue_connect_stmt = Statement(pattern=patterns.continue_connect,
+                                  action=ssh_continue_connecting,
+                                  args=None,
+                                  loop_continue=True,
+                                  continue_timer=False)
+
+hit_enter_stmt = Statement(pattern=patterns.hit_enter,
+                           action=wait_and_enter,
                            args=None,
                            loop_continue=True,
-                           continue_timer=True,
-                           trim_buffer=True)
+                           continue_timer=False)
+
+press_ctrlx_stmt = Statement(pattern=patterns.press_ctrlx,
+                             action=wait_and_enter,
+                             args=None,
+                             loop_continue=True,
+                             continue_timer=False)
+
+init_conf_stmt = Statement(pattern=patterns.setup_dialog,
+                           action='sendline(no)',
+                           args=None,
+                           loop_continue=True,
+                           continue_timer=False)
+
+mgmt_setup_stmt = Statement(pattern=patterns.enter_basic_mgmt_setup,
+                            action='send(\x03)',  # Ctrl-C
+                            args=None,
+                            loop_continue=True,
+                            continue_timer=False)
+
+clear_kerberos_no_realm = Statement(pattern=patterns.kerberos_no_realm,
+                                    action=sendline,
+                                    args=None,
+                                    loop_continue=True,
+                                    continue_timer=False)
+
+connected_stmt = Statement(pattern=patterns.connected,
+                           action=sendline,
+                           args=None,
+                           loop_continue=True,
+                           continue_timer=False)
+
+passphrase_stmt = Statement(pattern=patterns.passphrase_prompt,
+                            action=passphrase_handler,
+                            args=None,
+                            loop_continue=True,
+                            continue_timer=False)
+
+sudo_stmt = Statement(pattern=patterns.sudo_password_prompt,
+                      action=sudo_password_handler,
+                      args=None,
+                      loop_continue=True,
+                      continue_timer=False)
+
+syslog_msg_stmt = Statement(pattern=patterns.syslog_message_pattern,
+                            action=syslog_wait_send_return,
+                            args=None,
+                            loop_continue=True,
+                            trim_buffer=False,
+                            continue_timer=False)
+
+syslog_stripper_stmt = Statement(pattern=patterns.syslog_message_pattern,
+                                 action=syslog_stripper,
+                                 args=None,
+                                 loop_continue=True,
+                                 trim_buffer=False,
+                                 continue_timer=False)
+
+enter_your_selection_stmt = Statement(pattern=patterns.enter_your_selection_2,
+                                      action='sendline(2)',
+                                      args=None,
+                                      loop_continue=True,
+                                      continue_timer=True)
+
+press_any_key_stmt = Statement(pattern=patterns.press_any_key,
+                               action='sendline()',
+                               args=None,
+                               loop_continue=True,
+                               continue_timer=False)
+
+permission_denied_stmt = Statement(pattern=patterns.permission_denied,
+                                   action=permission_denied_handler,
+                                   args=None,
+                                   loop_continue=False,
+                                   continue_timer=False)
+
+terminal_position_stmt = Statement(pattern=patterns.get_cursor_position,
+                                   action=terminal_position_handler,
+                                   args=None,
+                                   loop_continue=True,
+                                   continue_timer=False)
