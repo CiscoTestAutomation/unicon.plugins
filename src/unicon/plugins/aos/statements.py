2b7c167260dc2e8122f8242d7dcea8702aee4a06
Alex Pfeil <apfeil@amfam.com>
2022-06-22 11:02:22 -0400

    commit

--- src/unicon/plugins/aos/statements.py
+++ src/unicon/plugins/aos/statements.py
@@ -6,7 +6,7 @@ https://github.com/CiscoDevNet/pyats-plugin-examples/tree/master/unicon_plugin_e
 '''
 from unicon.eal.dialogs import Statement
 from unicon.plugins.generic.statements import GenericStatements
-from .patterns import aosPatterns
+from unicon.plugins.aos.patterns import aosPatterns
 from unicon.plugins.generic.statements import enable_password_handler
 
 statements = GenericStatements()
@@ -36,7 +36,7 @@ enable_stmt = Statement(pattern=patterns.disable_mode,
                         continue_timer=False)
 
 password_stmt = Statement(pattern=patterns.password,
-                        action=None,
+                        action=sendline('exit\r'),
                         args=None,
                         loop_continue=True,
                         continue_timer=False)
