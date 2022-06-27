485ca99dec7c7ead72c4c864cbd160e569f7aaac
Alex Pfeil <apfeil@amfam.com>
2022-06-26 20:42:41 -0400

    C

--- src/unicon/plugins/aos/statements.py
+++ src/unicon/plugins/aos/statements.py
@@ -41,7 +41,7 @@ class aosStatements(object):
         
 # This is the statements to login to AOS.
         self.login_stmt = Statement(pattern=patterns.login_prompt,
-                                    action='sendline([password])',
+                                    action=common_cred_password_handler,
                                     args=None,
                                     loop_continue=True,
                                     continue_timer=True,
