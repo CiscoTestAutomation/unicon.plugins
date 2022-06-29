4d27e756ddb25ab9286800d26e868dbb6ac120d5
Alex Pfeil <apfeil@amfam.com>
2022-06-28 21:35:09 -0400

    C

--- src/unicon/plugins/aos/services.py
+++ src/unicon/plugins/aos/services.py
@@ -1,4 +1,32 @@
-class EOSServiceList(IosvServiceList):
+'''
+Author: Richard Day
+Contact: https://www.linkedin.com/in/richardday/, https://github.com/rich-day
+
+Contents largely inspired by sample Unicon repo:
+https://github.com/CiscoDevNet/pyats-plugin-examples/tree/master/unicon_plugin_example/src/unicon_plugin_example
+'''
+
+import logging
+
+from unicon.plugins.generic.service_implementation import Execute as GenericExec
+from unicon.plugins.ios.iosv import IosvServiceList
+
+logger = logging.getLogger(__name__)
+
+
+class Execute(GenericExec):
+    '''
+    Demonstrating how to augment an existing service by updating its call
+    service method
+    '''
+    def call_service(self, *args, **kwargs):
+        # custom... code here
+        #logger.info('execute service called')
+
+        # call parent
+        super().call_service(*args, **kwargs)
+
+class aosServiceList(IosvServiceList):
     '''
     class aggregating all service lists for this platform
     '''
