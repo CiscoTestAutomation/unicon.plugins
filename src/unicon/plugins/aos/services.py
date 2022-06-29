0c7c6c8c65b31e5148492fdfea849fc0b73c2d87
Alex Pfeil <apfeil@amfam.com>
2022-06-28 21:33:16 -0400

    C

---
+++ src/unicon/plugins/aos/services.py
@@ -0,0 +1,11 @@
+class EOSServiceList(IosvServiceList):
+    '''
+    class aggregating all service lists for this platform
+    '''
+
+    def __init__(self):
+        # use the parent servies
+        super().__init__()
+
+        # overwrite and add our own
+        self.execute = Execute
