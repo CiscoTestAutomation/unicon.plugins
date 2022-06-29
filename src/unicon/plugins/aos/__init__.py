748b2efad8c389f4727bae8eca16c6c89d5293bf
Alex Pfeil <apfeil@amfam.com>
2022-06-28 21:30:15 -0400

    C

--- src/unicon/plugins/aos/__init__.py
+++ src/unicon/plugins/aos/__init__.py
@@ -21,6 +21,5 @@ class aosSingleRPConnection(BaseSingleRpConnection):
     chassis_type = 'single_rp'
     state_machine_class = aosSingleRpStateMachine
     connection_provider_class = aosSingleRpConnectionProvider
-    subcommand_list = aosServiceList
     settings = aosSettings()
 
