b411017ed6edbeff7df6e1776ab9a50f8196e67f
Alex Pfeil <apfeil@amfam.com>
2022-06-28 21:24:22 -0400

    C

--- src/unicon/plugins/aos/patterns.py
+++ src/unicon/plugins/aos/patterns.py
@@ -7,15 +7,15 @@ https://github.com/CiscoDevNet/pyats-plugin-examples/tree/master/unicon_plugin_e
 class aosPatterns():
     def __init__(self):
         super().__init__()
-        self.shell_prompt = r'.+#$ ?$'
-        self.login_prompt = r'.*ogin.*$'
-        self.disable_mode = r'((.|\n)*>)$'
-        self.config_mode = r'.*config.#)$'
-        self.password = r'.*ssword:$'
-        self.linePassword = r'.*[Pp]assword:)$'
-        self.enable_prompt = r'.*#$'
-        self.config_prompt = r'.*config.*#)$'
-        self.proxy = r'.*rhome.*)$'
+        self.shell_prompt = r'.+#$'
+        self.login_prompt = r'.*ogin.*'
+        self.disable_mode = r'((.|\n)*>)'
+        self.config_mode = r'.*config.#)'
+        self.password = r'.*ssword:'
+        self.linePassword = r'.*[Pp]assword:'
+        self.enable_prompt = r'.*#'
+        self.config_prompt = r'.*config.*#)'
+        self.proxy = r'.*rhome.*)'
         self.escape_char = r"Escape character is '(~)'"
-        self.press_any_key = r'.*any key to conti.*$'
-        self.ssh_key_check = r'.*(yes/no/[fingerprint])?$'
+        self.press_any_key = r'.*any key to conti.*'
+        self.ssh_key_check = r'.*yes/no/[fingerprint]'
