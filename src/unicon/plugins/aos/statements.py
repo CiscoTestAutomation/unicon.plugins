c2e8047ccf431be2b937dc3fe4ab11d1b81ba713
Alex Pfeil <apfeil@amfam.com>
2022-06-30 19:27:17 -0400

    C

--- src/unicon/plugins/aos/statements.py
+++ src/unicon/plugins/aos/statements.py
@@ -25,37 +25,34 @@ def escape_char_handler(spawn):
 def run_level():
     sleep(1)
     spawn.read_update_buffer()
+    print("I hit the run_level")
     print(str(spawn.read_update_buffer()))    
-    
+
 def ssh_continue_connecting(spawn):
     """ handles SSH new key prompt
     """
     sleep(0.1)
+    print("I saw the ssh key configuration")
     spawn.sendline('yes')
 
 def wait_and_enter(spawn):
     # wait for 0.5 second and read the buffer
     # this avoids issues where the 'sendline'
     # is somehow lost
-    wait_time = timedelta(seconds=0.5)
-    settle_time = current_time = datetime.now()
-    while (current_time - settle_time) < wait_time:
-        spawn.read_update_buffer()
-        print(str(spawn.read_update_buffer()))
-        current_time = datetime.now()
+        print("I hit the enter key")
         spawn.sendline()
 
 class aosStatements(object):
     def __init__(self):
 
-
+        print()
 # This is the statements to login to AOS.
         self.start_stmt = Statement(pattern=patterns.start,
                                     action=run_level,
                                     args=None,
                                     loop_continue=True,
                                     continue_timer=True,
-                                    trim_buffer=False,
+                                    trim_buffer=True,
                                     debug_statement=True)
         self.login_stmt = Statement(pattern=patterns.login_prompt,
                                     action=login_handler,
@@ -70,27 +67,21 @@ class aosStatements(object):
                                        loop_continue=True,
                                        continue_timer=True,
                                        trim_buffer=False,
-                                       debug_statement=True,
-                                       matched_retries=3,
-                                       matched_retry_sleep=1)
+                                       debug_statement=True)
         self.ssh_key_check = Statement(pattern=patterns.proxy,
                                     action=ssh_continue_connecting,
                                     args=None,
                                     loop_continue=True,
                                     continue_timer=True,
                                     trim_buffer=False,
-                                    debug_statement=True,
-                                    matched_retries=3,
-                                    matched_retry_sleep=1)
+                                    debug_statement=True)
         self.press_any_key_stmt = Statement(pattern=patterns.press_any_key,
                                             action=wait_and_enter,
                                             args=None,
                                             loop_continue=True,
                                             continue_timer=True,
                                             trim_buffer=False,
-                                            debug_statement=True,
-                                            matched_retries=3,
-                                            matched_retry_sleep=1)       
+                                            debug_statement=True)       
 
 #############################################################
 #  Statement lists
