b9291e69404a62b2b8340d655fde1498a488e3e2
Alex Pfeil <apfeil@amfam.com>
2022-09-29 19:25:02 -0400

    Updated init file

--- src/unicon/plugins/aos/__init__.py
+++ src/unicon/plugins/aos/__init__.py
@@ -14,15 +14,8 @@ from unicon.plugins.aos.services import aosServiceList
 from unicon.plugins.aos.settings import aosSettings
 from .statemachine import aosSingleRpStateMachine
 
-#This enables logging in the script.
-import logging
-#Logging disable disables logging in the script. In order to turn on logging, comment out logging disable.
-logging.disable(logging.DEBUG)
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
 #Checking to see if this is necessary. I will most likely take this out.
 def wait_and_send_yes(spawn):
-    logging.debug('init wait and send yes(%s)')
     time.sleep(0.2)
     spawn.sendline('yes')
 
@@ -32,17 +25,10 @@ class aosSingleRPConnection(BaseSingleRpConnection):
     
     This supports logging into an Aruba switch.
     '''
-    logging.debug('***init aosSingleRPConnection called(%s)***')
     os = 'aos'
-    logging.debug('***init os statement passed(%s)***')
     chassis_type = 'single_rp'
-    logging.debug('***init chassis type passed(%s)***')
     state_machine_class = aosSingleRpStateMachine
-    logging.debug('***init state machine class loaded(%s)***')
     connection_provider_class = aosSingleRpConnectionProvider
-    logging.debug('***init Connection Provider Loaded(%s)***')
     subcommand_list = aosServiceList
-    logging.debug('***init Service List Loaded(%s)***')
     settings = aosSettings()
-    logging.debug('***init Settings Loaded(%s)***')
 
