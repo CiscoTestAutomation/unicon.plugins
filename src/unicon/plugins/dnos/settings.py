9a0e0c1ceec4054560c69c3fea49b241aed3d366
nskalis <nskalis.apps@gmail.com>
2023-01-30 21:30:32 +0100

    dnos: initial draft - StateMachineError: enable state is not registered

---
+++ src/unicon/plugins/dnos/settings.py
@@ -0,0 +1,23 @@
+'''
+Connection Settings
+-------------------
+
+Connection settings are basically various key/value pairs that controls the
+default behavior of a connection. 
+
+'''
+
+from unicon.plugins.generic.settings import GenericSettings
+
+
+class DnosSettings(GenericSettings):
+ 
+    def __init__(self):
+        # inherit any parent settings
+        super().__init__()
+
+        # and modify some for our own
+        self.CONNECTION_TIMEOUT = 60*3
+
+        # and we could add more - to be used in plugins if needed
+        # self.<keyword> = <value>
---
+++ src/unicon/plugins/dnos/statemachine.py
@@ -0,0 +1,38 @@
+'''
+Connection Statemachine
+-----------------------
+
+The connection state machine holds the details of all supported states of a 
+given platform, and handles the migration of the device from current state to
+any possible next state.
+
+The state machineclass provides a create method where all the device states 
+have to be created. State machine should be subclass of StateMachine class 
+from unicon.statemachine.
+'''
+
+from unicon.statemachine import State, Path, StateMachine
+# from unicon.eal.dialogs import Statement, Dialog
+from unicon.plugins.dnos.patterns import DnosPatterns
+
+
+pat = DnosPatterns()
+
+
+class DnosSingleRpStateMachine(StateMachine):
+
+    def create(self):
+        '''
+        statemachine class's create() method is its entrypoint. This showcases
+        how to setup a statemachine in Unicon. 
+        '''
+        operation = State('operation', pat.operation_prompt)
+        self.add_state(operation)
+        # configuration = State('configuration', pat.configuration_prompt)
+        # self.add_state(configuration)
+
+        # operation_to_configuration = Path(operation, configuration, 'configure')
+        # self.add_path(operation_to_configuration)
+        # configuration_to_operation = Path(configuration, operation, 'exit')
+        # self.add_path(configuration_to_operation)
+        
