e5efd24887db0ebc9a05bcdc1784eeda30c69a29
nskalis <nskalis.apps@gmail.com>
2023-03-08 07:16:38 +0100

    .

--- src/unicon/plugins/dnos/patterns.py
+++ src/unicon/plugins/dnos/patterns.py
@@ -7,7 +7,7 @@ regex patterns that matches dialogs, statements & the likes into one location.
 
 '''
 
-unicon.plugin.generic.patterns import GenericPatterns
+from unicon.plugin.generic.patterns import GenericPatterns
 
 
 class DnosPatterns(GenericPatterns):
